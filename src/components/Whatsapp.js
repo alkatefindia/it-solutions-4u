62ec45c308b1ad5ae65fe460f077ca25d0de2243
Arjun P <arjunpknr0016@gmail.com>
2023-12-26 15:48:46 +0530

    premsg updated

--- src/components/Whatsapp.js
+++ src/components/Whatsapp.js
@@ -4,7 +4,7 @@ import whatsapp from "../images/18ba0e411b1586feb65e97d7deb45b5d.png"
 
 const Whatsapp = ({premsg}) => {
   const phoneNumber = '+971556449916';
-  const predefinedMessage = {premsg};
+  const predefinedMessage = premsg || '';
 
     
       const whatsappLink = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(predefinedMessage)}`;
