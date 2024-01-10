e2cc4ed13df32bc8c843eff302b4820ef42577d0
Arjun P <arjunpknr0016@gmail.com>
2024-01-10 17:30:17 +0530

    app.js content removed

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { hydrate, render } from "react-dom";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,9 +9,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-const rootElement = document.getElementById("root");
-if (rootElement.hasChildNodes()) {
-  hydrate(<App />, rootElement);
-} else {
-  render(<App />, rootElement);
-}
